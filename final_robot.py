1141b39f7cdea7689aa5b62a0fccf0eaa04e6269
Enes BILICI <97558462+bilicderler@users.noreply.github.com>
2024-01-24 17:07:29 +0300

    Üniversite dersi kapsamında yapılmış, pybullet simülasyon örneğidir.

---
+++ final_robot.py
@@ -0,0 +1,19 @@
+﻿
+import pybullet as p
+import pybullet_data
+import time
+
+p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+plane = p.loadURDF("plane.urdf")
+robot = p.loadURDF("final_robot.urdf")
+
+p.setGravity(0, 0, -10)
+
+# 2. eklemi kontrol modunu ve hedef pozisyonunu belirle
+joint_index = 1  # 2. eklem için indeks 1 olacaktır
+p.setJointMotorControl2(robot, joint_index, p.POSITION_CONTROL, targetPosition = 1)
+
+while True:
+    p.stepSimulation()
+    time.sleep(0.10)
